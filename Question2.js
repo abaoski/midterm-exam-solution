09b14af29ac9c8d00116598eda70a120557f8cd7
Abao <christopherabao12@gmail.com>
2025-03-14 19:43:03 +0800

    Question2 fix name  <3

--- Question2.js
+++ Question2.js
@@ -1,7 +1,10 @@
+// function that prints a greeting to the console after a short delay
 function delayedGreeting(name) {
+    // setTimeout makes the greeting appear after 2 milliseconds 
     setTimeout(() => {
       console.log(`Hello, ${name}!`);
     }, 2); 
-  }
-  
-  delayedGreeting("Alice")
+}
+
+// Try
+delayedGreeting("Alice");
